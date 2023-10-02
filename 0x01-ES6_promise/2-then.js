67c0909fb16efaabf0205918604406c18b89327e
Olalekan Otaoroju <otaorojuolalekan@gmail.com>
2023-10-02 14:02:33 +0100

    task 2

--- 0x01-ES6_promise/2-then.js
+++ 0x01-ES6_promise/2-then.js
@@ -1,15 +1,6 @@
 export default function handleResponseFromAPI(promise) {
-  const newpromise = new Promise((myResolve, myReject) => {
-    if (promise) {
-      myResolve({
-        status: 200,
-        body: 'success',
-      });
-    }
-
-    myReject(new Error());
-  });
-  newpromise.then(() => {
-    console.log('Got a response from the API');
-  });
+  return promise
+    .then(() => ({ status: 200, body: 'success' }))
+    .catch(new Error())
+    .finally(console.log('Got a response from the API'));
 }
