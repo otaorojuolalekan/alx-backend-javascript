ef5339605f257e3874f249490b2da7ddabcc1f2c
Olalekan Otaoroju <otaorojuolalekan@gmail.com>
2023-10-07 22:13:25 +0100

    trying to fix bug in task 3

--- 0x02-ES6_classes/3-currency.js
+++ 0x02-ES6_classes/3-currency.js
@@ -4,24 +4,30 @@ export default class Currency {
     this.name = name;
   }
 
+  displayFullCurrency() {
+    return (`${this._name} (${this._code})`);
+  }
+
   // getter and setter functions
   get code() {
     return this._code;
   }
 
-  set code(codeval) {
-    this._code = codeval;
+  set code(codeVal) {
+    if (typeof codeVal !== 'string') {
+      throw new TypeError('code must be a string');
+    }
+    this._code = codeVal;
   }
 
   get name() {
     return this._code;
   }
 
-  set name(nameval) {
-    this._name = nameval;
-  }
-
-  displayFullCurrency() {
-    return (`${this._name} (${this._code})`);
+  set name(nameVal) {
+    if (typeof nameVal !== 'string') {
+      throw new TypeError('name must be a string');
+    }
+    this._name = nameVal;
   }
 }
